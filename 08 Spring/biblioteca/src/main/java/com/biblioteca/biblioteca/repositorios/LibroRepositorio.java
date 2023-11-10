d20db4d14a78eab1db46f415936094cdd623b000
Dario Cardozo <decardo1982@gmail.com>
2023-11-09 21:48:24 -0300

    armando biblioteca con los videos

---
+++ 08 Spring/biblioteca/src/main/java/com/biblioteca/biblioteca/repositorios/LibroRepositorio.java
@@ -0,0 +1,19 @@
+package com.biblioteca.biblioteca.repositorios;
+
+import com.biblioteca.biblioteca.entidades.Libro;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LibroRepositorio extends JpaRepository<Libro, Long> {
+
+    @Query("SELECT l FROM Libro l WHERE l.titulo = :titulo")
+    public Libro buscarPorTitulo(@Param("titulo") String titulo); //el param titulo hace referencia a l.titulo y String titulo hace referencia a :titulo
+    
+    @Query("SELECT l FROM Libro l WHERE l.autor.nombre = :nombre")
+    public List<Libro> buscarPorAutor(@Param("nombre") String nombre); //
+
+}
